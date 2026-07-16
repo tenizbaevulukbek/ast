a79d47af18edabaa5263739c365607a39f582ba9
Ulukbek <tenizbaevuk@gmail.com>
2026-07-16 11:20:36 +0600

    changing image

--- components/ScrollStory.tsx
+++ components/ScrollStory.tsx
@@ -6,7 +6,7 @@ import React, { useRef, useEffect, useState, useCallback } from "react";
 const SLIDES = [
   {
     image:
-      "https://images.unsplash.com/photo-1694327671725-e2a81cda3436?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      "https://images.unsplash.com/photo-1497440001374-f26997328c1b?q=80&w=1632&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     image:
